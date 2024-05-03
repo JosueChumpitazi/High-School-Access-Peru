e744ce9fad255d127b1cce15ffbb49bcce4fd7ac
Rodrigo Grijalba <ragrijalbat@gmail.com>
2024-05-02 21:25:40 -0500

    base app

---
+++ README.md
@@ -0,0 +1,7 @@
+## Simpson's Paradox Dashboard with Python
+
+This dashboard was based off of the dashboard made by [Bach and Tan](https://github.com/DigitalCausalityLab/simpsonsparadox) to demonstrate the same phenomenon. This example is taken from Glymour et al. (2016)
+
+### Refferences
+
+Glymour, Madelyn, Judea Pearl, and Nicholas P. Jewell. Causal inference in statistics: A primer. John Wiley & Sons, 2016. 
---
+++ src/streamlit_app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from estimation import execute_regressions, generate_data
+from plots import *
+
+data = generate_data()
+results, results_data = execute_regressions(data)
+
+st.set_page_config(page_title="Simpson's Paradox", layout="wide")
+
+tab1, tab2, tab3 = st.tabs(["Simpson's Paradox", "Code", "References"])
+
+with tab1:
+
+        col1, col2 = st.columns([1, 2])
+        st.write("---")
+
+        with col1:
+                st.markdown("### Options")
+                segregated = st.checkbox(label="Segregate by age")
+                fit_line = st.checkbox(label="Show regression line", value=True)
+                st.write("---")
+                
+
+        fig = get_figure(results_data, segregated, fit_line, )
+        table = get_table(results, segregated)
+
+        with col1:
+                st.markdown("### Regression Results")
+                st.table(table)
+
+
+
+        with col2:
+                st.markdown("### Scatter Plot")
+                st.plotly_chart(fig, use_container_width=True)
+                st.write("---")
+                st.markdown("### DAG")
+                if segregated:
+                        st.image("assets/segregated.png")
+                else:
+                        st.image("assets/aggregated.png")
+
+with tab2:
+        st.markdown("""
+### Code
+
+Our code is hosted in the following GithHub repository: 
+""")
+
+
+
